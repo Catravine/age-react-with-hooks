5b843de9883c9746875b7ef799df57a1e55de71f
Caroline Courtney <caroline@carolinecourtney.com>
2019-11-10 13:35:29 -0500

    simple setting an age in state with useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [age, setAge] = useState(21);
+  const ageUpHandle = () => {
+    setAge(age + 1);
+  }
+  const ageDownHandle = () => {
+    setAge(age - 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Use State Hook</h1>
+        <h2>Age: {age}</h2>
+        <button onClick={ageUpHandle}>Age up</button>
+        <button onClick={ageDownHandle}>Age down</button>
       </header>
     </div>
   );
